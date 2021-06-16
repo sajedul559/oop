37510afa4da99e72153d1137c6661174ff4300b6
sajedul <sajedulkhairul@gmail.com>
2021-06-16 20:24:50 +0600

    add staticmethod code

---
+++ staticmethod.php
@@ -0,0 +1,30 @@
+<?php 
+
+class sajedul{
+	public static function khairul(){
+		echo"My name is sajedul islam";
+	}
+
+}
+Fruit:: mango();
+
+
+class Fruit{
+	public static function mango(){
+		echo" Mango is popular fruit in bangladesh<br>";
+	}
+}
+
+sajedul:: khairul();
+
+
+
+
+
+
+
+
+
+
+
+?>
